073926518030b218e036322c3993cadcf8d2977a
Admin <subashbaidya9@gmail.com>
2023-02-01 19:31:34 +0545

    Home Page with Products component

--- redux-toolkit/src/pages/Home.js
+++ redux-toolkit/src/pages/Home.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Products from '../components/Products'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h2>Welcome to REDUX_TOOLKIT STORE</h2>
+      <section>
+        <h3>PRODUCTS</h3>
+        <Products/>
+      </section>
+    </div>
   )
 }
 
